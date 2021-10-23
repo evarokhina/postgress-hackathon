4d7144d5804afe23674ddee547d66e7f05db12e3
sltls <kasimlogon@gmail.com>
2021-10-23 14:49:04 +0300

    pre-alfa

--- backend/js/index.js
+++ backend/js/index.js
@@ -1,11 +1,35 @@
+let elem = document.getElementById('output-one');
+elem.innerHTML = "text";
 
-    let output_text = document.getElementById('output-one');
-    let output_text2 = document.getElementById('output-two');
 
-    let promise = fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: output_text2.innerHTML = "Хуй",
-    });
+// async function gunc() {
+//     let test = {
+//         test1: 'test1',
+//         test2: 'test2'
+//     };
+//
+//     let promise = await fetch('/backend/js/index.js/test', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8'
+//         },
+//         body: JSON.stringify(test)
+//     });
+//
+//     let result = await promise.json();
+//     alert(result.message);
+// }
+
+const gunc = event => {
+        let promise = await fetch('/backend/js/index.js/test', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(test)
+        });
+};
+
+const submitEl = document.getElementById('button');
+
+submitEl.addEventListener('click', gunc);
